d570a853a2825487ea71b4f1eb15c21440873310
wangzz <wzzvictory_tjsd@163.com>
2014-05-22 19:27:43 +0800

    modify

--- Attendance/FGAppDelegate.m
+++ Attendance/FGAppDelegate.m
@@ -26,6 +26,8 @@
     if (!result) {
         NSLog(@"write to file error.");
     }
+    
+    [self formatAttendanceDaceWith:attendanceArr];
 }
 
 - (NSArray *)readFile
@@ -60,11 +62,26 @@
         
         person.arriveTime = [currentContentArr objectAtIndex:4];
         
-        [attendanceArr addObject:person];
+        if (person.name == nil || person.name.length == 0) {
+            NSLog(@"err");
+        } else {
+            [attendanceArr addObject:person];
+        }
+        
     }
     
     return attendanceArr;
 }
+
+
+- (void)formatAttendanceDaceWith:(NSArray *)arr
+{
+    NSArray *results = [arr valueForKeyPath:@"@distinctUnionOfObjects.name"];
+    
+    NSLog(@"results:%@",results);
+}
+
+
 
 - (BOOL)writeToCSVFile:(NSArray *)arr
 {
@@ -72,33 +89,13 @@
     NSMutableData *data = [NSMutableData data];
     for (FGPerson *person in arr) {
         NSString *string = [NSString stringWithFormat:@"%@\t%@\t%@\t%@\n",person.ID,person.employeeID,person.name,person.arriveTime];
-        NSLog(@"%@",string);
+//        NSLog(@"%@",string);
         [data appendData:[string dataUsingEncoding:NSUTF16StringEncoding]];
     }
     
     return [data writeToFile:path atomically:YES];
 }
 
-/*
- 
- 转义字符               意义                    ASCII码值（十进制）
- \a         响铃(BEL)                             007
- \b         退格(BS) ，将当前位置移到前一列           008
- \f         换页(FF)，将当前位置移到下页开头          012
- \n         换行(LF) ，将当前位置移到下一行开头       010
- \r         回车(CR) ，将当前位置移到本行开头         013
- \t         水平制表(HT) （跳到下一个TAB位置）        009
- \v         垂直制表(VT)	011
- \\         代表一个反斜线字符''\'                   092
- 
- \'         代表一个单引号（撇号）字符                039
- \"         代表一个双引号字符                       034
- \0         空字符(NULL)                           000
- \ddd       1到3位八进制数所代表的任意字符            三位八进制
- \xhh       1到2位十六进制所代表的任意字符            二位十六进制
-
- 
- */
 
 
 
